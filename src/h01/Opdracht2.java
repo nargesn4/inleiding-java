83885bac396b90bde52872f17406df37c91506a5
Narges jafari <nargesjafarie@gmail.com>
2018-08-29 12:41:43 +0200

    opdracht2.2

---
+++ src/h01/Opdracht2.java
@@ -0,0 +1,16 @@
+package h01;
+
+import java.awt.*;
+
+public class Opdracht2 extends Show {
+    public void init() {
+        setBackground(Color.white);
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Narges", 100, 40 );
+        g.setColor(Color.red);
+        g.drawString("Jafari", 100, 60 );
+    }
+}
---
+++ src/h01/Show.java
@@ -0,0 +1,16 @@
+package h01;
+import java.awt.*;
+import java.applet.*;
+
+public class Show extends Applet {
+
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+        setBackground(Color.blue);
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Welcome to Java!!", 100, 60 );
+    }
+}

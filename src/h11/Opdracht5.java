df47e867e6640d9e6d205c3be42b060b6e71b5e0
Narges jafari <nargesjafarie@gmail.com>
2018-09-03 13:57:06 +0200

    h11 opdracht5

---
+++ src/h11/Opdracht5.java
@@ -0,0 +1,24 @@
+package h11;
+
+import h01.Show;
+
+import java.awt.*;
+
+public class Opdracht5 extends Show {
+    int x, y;
+
+    public void init() {
+        x = 20;
+        y = 20;
+    }
+
+
+    public void paint(Graphics g) {
+        for (int i = 0; i < 5; i++) {
+            g.drawRect(x, y, 40, 40);
+            x+=40;
+            y+=40;
+            
+        }
+    }
+}
